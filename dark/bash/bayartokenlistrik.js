7a0371be1c4247a20f20d2fee77644a36403fcd6
testevrzn <78117358+testevrzn@users.noreply.github.com>
2021-03-01 03:10:34 -0300

    Add files via upload

---
+++ dark/bash/bayartokenlistrik.js
@@ -0,0 +1,14 @@
+const bayartokenlistrik = (prefix) => { 
+	return `
+	
+	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Vrau Bot*
+	
+[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do Youtube : Em Breve*
+
+⧿⧽ *Não se esqueça de se inscrever*
+⧿⧽ *Ative os sinos*
+⧿⧽ *Atenciosamente Vrau*
+
+`
+}
+exports.bayartokenlistrik = bayartokenlistrik
